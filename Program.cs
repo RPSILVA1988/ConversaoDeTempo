28c031f060ce19581f2c6a5292c56371c29c1b4a
RANIELE SILVA <comraniele@hotmail.com>
2021-03-07 18:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConversaoDeTempo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int N, horas, resto, minutos, segundos;
+
+			N = int.Parse(Console.ReadLine());
+
+			horas = N / 3600;
+			resto = N % 3600;
+
+			minutos = resto / 60;
+			segundos = resto % 60;
+
+			Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+			Console.ReadLine();
+		}
+	}
+}
